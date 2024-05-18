d9a8d2b94501a4f275626af4c9a3acd230c0b269
Jesus Cruz Verdecia <jesus.cruz.verdecia@gmail.com>
2024-05-18 18:28:17 -0400

    Add firebase and swr data fetching to packages
    Add firebase configuration

---
+++ app/firebase/firebaseConfig.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkIZfz9BvXs_cnwBEr1ICtkBZ7WNvN3co",
+  authDomain: "todo-app-8cee4.firebaseapp.com",
+  projectId: "todo-app-8cee4",
+  storageBucket: "todo-app-8cee4.appspot.com",
+  messagingSenderId: "776675867031",
+  appId: "1:776675867031:web:aa6111bd8aa5869dadecd2",
+  measurementId: "G-KXFKBVDYZF",
+};
+
+// Initialize Firebase
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
