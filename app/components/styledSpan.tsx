6297da58f3ab02c4cc6caf67bff68531ab77e147
Jesus Cruz Verdecia <jesus.cruz.verdecia@gmail.com>
2024-05-18 20:27:41 -0400

    Update component with wrong state

---
+++ app/components/styledSpan.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+
+//material components
+import Typography from "@mui/material/Typography";
+import { useTheme } from "@mui/material/";
+
+type StyledSpanProps = {
+  variant?: "hashtag" | "email" | "mention" | "link";
+  children: React.ReactNode;
+};
+
+const StyledSpan: React.FC<StyledSpanProps> = ({ variant, children }) => {
+  const theme = useTheme();
+  if (variant) {
+    return (
+      <Typography component="span" color={theme.decorators[variant].text}>
+        {children}
+      </Typography>
+    );
+  } else {
+    return <Typography component="span">{children}</Typography>;
+  }
+};
+export default StyledSpan;
